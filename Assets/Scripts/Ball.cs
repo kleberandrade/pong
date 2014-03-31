ba37ff94c5d01c071fd4b20f70c2260918be2cac
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2014-03-31 14:52:55 -0300

    Add Player and Ball scripts.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,15 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Ball : MonoBehaviour {
+[RequireComponent (typeof(Rigidbody), typeof(TrailRenderer))]
+public class Ball : MonoBehaviour 
+{
+    [SerializeField]
+    private float speed = 3.0f;
+    [SerializeField]
+    private float trailTimer = 0.8f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private Vector3 origin;
+    private Vector3 leftImpulse = new Vector3(1.0f, 0.0f, 1.0f);
+    private Vector3 rightImpulse = new Vector3(-1.0f, 0.0f, -1.0f);
+
+    private TrailRenderer trail;
+
+    void Start()
+    {
+        origin = transform.position;
+        trail = GetComponent<TrailRenderer>();
+        trail.time = trailTimer;
+    }
+
+    void StartImpulse(Vector3 side)
+    {
+        trail.time = 0.0f;
+        rigidbody.velocity = Vector3.zero;
+        side.y = Random.Range(-1.0f, 1.0f) < 0.0f ? -1.0f : 1.0f;
+        transform.position = origin;
+        rigidbody.AddForce(side * speed, ForceMode.Impulse);
+        trail.time = trailTimer;
+    }
+
+    void LeftStartImpulse()
+    {
+        StartImpulse(leftImpulse);
+    }
+
+    void RightStartImpulse()
+    {
+        StartImpulse(rightImpulse);
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+[RequireComponent (typeof(Rigidbody))]
+public class Player : MonoBehaviour 
+{
+    [SerializeField]
+    private float speed = 15.0f;
+    [SerializeField]
+    private PlayerType type;
 
-	// Use this for initialization
-	void Start () {
-	
+    private float move;
+
+	void Update () 
+    {
+        // Left Player
+	    if (type == PlayerType.Left)
+        {
+#if UNITY_EDITOR || UNITY_WEBPLAYER || UNITY_STANDALONE
+            move = Input.GetAxis("VerticalLeft") * speed;
+#elif UNITY_ANDROID
+
+#endif
+        }
+
+        // Right Player
+        if (type == PlayerType.Right)
+        {
+#if UNITY_EDITOR || UNITY_WEBPLAYER || UNITY_STANDALONE
+            move = Input.GetAxis("VerticalRight") * speed;
+#elif UNITY_ANDROID
+
+#endif
+        }
+
+        // Artificial Intelligence Player
+        if (type == PlayerType.AI)
+        {
+
+        }
+
+        rigidbody.velocity = Vector3.forward * move;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+}
+
+public enum PlayerType
+{
+    Left,
+    Right,
+    AI
 }
