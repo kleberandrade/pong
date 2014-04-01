43101fb73326b13419d4ca68ae1e07a121003b17
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2014-04-01 00:55:02 -0300

    Added lights.

---
+++ Assets/Scripts/Foregraound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Foregraound : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 2.0f;
+
+	void Update () 
+    {
+        transform.localScale = new Vector3(
+            7.0f + Mathf.PingPong(Time.time * speed, 3),
+            1.0f,
+            7.0f + Mathf.PingPong(Time.time * speed, 3));
+	}
+}
